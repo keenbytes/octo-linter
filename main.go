ec72777a37ca81bcab69d54601d6327a1719735d
Mikolaj Gasior <mg@computerclub.pl>
2025-05-03 20:26:59 +0200

    Bump broccli to v3

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -8,33 +9,33 @@ import (
 	"gopkg.pl/mikogs/octo-linter/pkg/dotgithub"
 	"gopkg.pl/mikogs/octo-linter/pkg/linter"
 	"gopkg.pl/mikogs/octo-linter/pkg/loglevel"
-	"gopkg.pl/phings/broccli/v2"
+	"gopkg.pl/phings/broccli/v3"
 )
 
 func main() {
-	cli := broccli.NewCLI("octo-linter", "Validates GitHub Actions workflow and action YAML files", "mg@computerclub.pl")
+	cli := broccli.NewBroccli("octo-linter", "Validates GitHub Actions workflow and action YAML files", "m@gasior.dev")
 
-	cmd := cli.AddCmd("lint", "Runs the linter on files from a specific directory", lintHandler)
-	cmd.AddFlag("path", "p", "DIR", "Path to .github directory", broccli.TypePathFile, broccli.IsDirectory|broccli.IsExistent|broccli.IsRequired)
-	cmd.AddFlag("config", "c", "FILE", "Linter config with rules in YAML format", broccli.TypePathFile, broccli.IsRegularFile|broccli.IsExistent)
-	cmd.AddFlag("loglevel", "l", "", "One of NONE,ERR,WARN,DEBUG", broccli.TypeString, 0)
-	cmd.AddFlag("vars-file", "z", "", "Check if variable names exist in this file (one per line)", broccli.TypePathFile, broccli.IsExistent)
-	cmd.AddFlag("secrets-file", "s", "", "Check if secret names exist in this file (one per line)", broccli.TypePathFile, broccli.IsExistent)
+	cmd := cli.Command("lint", "Runs the linter on files from a specific directory", lintHandler)
+	cmd.Flag("path", "p", "DIR", "Path to .github directory", broccli.TypePathFile, broccli.IsDirectory|broccli.IsExistent|broccli.IsRequired)
+	cmd.Flag("config", "c", "FILE", "Linter config with rules in YAML format", broccli.TypePathFile, broccli.IsRegularFile|broccli.IsExistent)
+	cmd.Flag("loglevel", "l", "", "One of NONE,ERR,WARN,DEBUG", broccli.TypeString, 0)
+	cmd.Flag("vars-file", "z", "", "Check if variable names exist in this file (one per line)", broccli.TypePathFile, broccli.IsExistent)
+	cmd.Flag("secrets-file", "s", "", "Check if secret names exist in this file (one per line)", broccli.TypePathFile, broccli.IsExistent)
 
-	_ = cli.AddCmd("version", "Prints version", versionHandler)
+	_ = cli.Command("version", "Prints version", versionHandler)
 	if len(os.Args) == 2 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
 		os.Args = []string{"App", "version"}
 	}
 
-	os.Exit(cli.Run())
+	os.Exit(cli.Run(context.Background()))
 }
 
-func versionHandler(c *broccli.CLI) int {
+func versionHandler(ctx context.Context, c *broccli.Broccli) int {
 	fmt.Fprintf(os.Stdout, VERSION+"\n")
 	return 0
 }
 
-func lintHandler(c *broccli.CLI) int {
+func lintHandler(ctx context.Context, c *broccli.Broccli) int {
 	logLevel := loglevel.GetLogLevelFromString(c.Flag("loglevel"))
 	varsFile := c.Flag("vars-file")
 	secretsFile := c.Flag("secrets-file")
